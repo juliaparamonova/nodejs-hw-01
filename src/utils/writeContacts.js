11b6b76ccfa072246ea54e9a120ae21386a2120d
Julia <jpkoord@gmail.com>
2024-09-27 18:44:20 +0300

    add contacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async contacts =>
+  fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
